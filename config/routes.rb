525be67d3083e14c922a6629b62b40de10a0d108
Amar Gill <amardeep@clearbanc.com>
2023-01-15 23:21:32 -0500

    setup articles as a resource. tidy up articles/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "articles#index"
-
   # means GET /articles mapped to index action (method) on ArticlesController
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
