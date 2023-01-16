69dc331273eea3f39bd34209fd30148f4bdb4911
Amar Gill <amardeep@clearbanc.com>
2023-01-15 22:32:02 -0500

    render article titles on page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   # Rails will automatically render a view that matches
   # the name of the controller and action
   def index
+    @articles = Article.all
   end
 end
