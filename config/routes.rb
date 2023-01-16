e2e15a269a0087f458957f89cb8c7125c600e424
Amar Gill <amardeep@clearbanc.com>
2023-01-15 22:59:43 -0500

    can show article on own page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # means GET /articles mapped to index action (method) on ArticlesController
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 
   # Defines the root path route ("/")
   # root "articles#index"
